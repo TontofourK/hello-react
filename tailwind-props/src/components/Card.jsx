57cd1fe77423e6bc8fe06d01f5e5f1e8c880b975
Kapish Dahiya <kapish1233@gmail.com>
2024-02-26 07:04:00 +0530

    completed tailwind props

---
+++ tailwind-props/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <img src="https://imgs.search.brave.com/DdnXbTaUuOwj0ImwY49kqJusC6P2yLFzfbyIthODocE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/dGhlLWJlc3QtY291/cGxlLWJ5LXYwLWdq/Y2tlMnhneXFrYzEu/anBlZz93aWR0aD02/NDAmY3JvcD1zbWFy/dCZhdXRvPXdlYnAm/cz0yYWM2YjU1YmUz/M2QwZWQ1MWU0ZWY1/MDQ2MzEzNDk1Yjlh/N2VmMDgx" alt="" />
+        <h1 class='text-2xl bg-green-500 p-3 rounded'>
+            A card for photo
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, autem.
+        </p>
+    </div>
+  )
+}
+
+export default Card
