e56045849d6c55047161746b60c69895aeac3b06
Kapish Dahiya <kapish1233@gmail.com>
2024-03-17 09:55:32 +0530

    buggy currency converter

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { InputBox } from "./components/index.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setamount] = useState(0);
+  const [from, setFrom] = useState("inr");
+  const [to, setTo] = useState("usd");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'"
+        style={{
+          backgroundImage: `url(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fd%2Fdc%2FMoney_and_pills_in_three_colors.jpg&f=1&nofb=1&ipt=b2f805ef45877c74f4c5183388d29eb25485c5b22ae51df1cb14141fdbf68215&ipo=images)`,
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border boder-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="from"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  onAmountChange={(amount) => setamount(amount)}
+                  selectedCurrency={from}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  className="absolute left-1/2 -translate-x-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  Swap
+                </button>
+              </div>
+              <div className="w-full mb-1">
+                <InputBox
+                  label="to"
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectedCurrency={to}
+                  amountDisabled
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
