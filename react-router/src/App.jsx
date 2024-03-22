e27a83f7aeefe3de64b9c9f2eb1d6e748093249a
Kapish Dahiya <kapish1233@gmail.com>
2024-03-22 10:09:01 +0530

    react-router in-progress

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
   return (
     <>
-    <h1 className='text-black font-bold bg-blue-200 text-3xl'>Hello World</h1>
+      <h1 className="text-black font-bold bg-blue-200 text-3xl">Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Layout from './Layout.jsx'
-import Home from './components/home/Home.jsx'
-import About from './components/about/About.jsx'
+import Layout from "./Layout.jsx";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
+import ContactUs from "./components/contact-us/ContactUs.jsx"
 import {
   Route,
   createRoutesFromElements,
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import User from "./components/user/User.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
       <Route path="about/" element={<About />} />
+      <Route path="contact-us/" element={<ContactUs/>} />
+      <Route path="user/" element={<User />} > 
+        <Route path=":userid" element={<User />} />
+      </Route>
     </Route>
   )
 );
