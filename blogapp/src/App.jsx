33de4394f2b6ffa942d312bdb9ae392f77247c66
Kapish Dahiya <kapish1233@gmail.com>
2024-06-16 14:46:17 +0530

    started blogapp

---
+++ blogapp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className='text-red-300 bg-black/50'>Hello negga</h1>
+   </> 
+  )
+}
+
+export default App
